f20dcb95c0db90648c73e5ce29284c08e1ffcbea
Arthur Carvalho Gomes Junior <arthur360thug@gmail.com>
2022-07-07 14:50:12 +0000

    Commit inicial apenas com as funcionalidades a fazer

---
+++ main.py
@@ -0,0 +1,19 @@
+board = ["-","-","-",
+        "-","-","-",
+        "-","-","-",]
+
+
+
+
+     # functionalities
+# board
+# display board
+# play game
+# handle turn
+# check wins
+  # check rows
+  # check columns
+  # check diagonals
+# check tie
+# flip player
+
